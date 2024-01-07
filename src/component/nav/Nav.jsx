62d8021d7133d2c1f38952c80bacd1dd511be070
Janamjay <janamjayk6@gmail.com>
2024-01-07 10:51:04 +0530

    user

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -4,10 +4,14 @@ import navStyles from "./Nav.module.css";
 import { useSelector } from "react-redux";
 import { RiMenuFoldFill, RiCloseFill } from "react-icons/ri";
 import { HiShoppingCart } from "react-icons/hi";
+import { CgProfile } from "react-icons/cg";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const isLoggedIn = localStorage.getItem("login-success") === "true";
+  const userData = localStorage.getItem("currentUser");
+  const currentUser = JSON.parse(userData);
+  const userName = currentUser?.name;
   const data = useSelector((state) => state.cart);
 
   const toggleMenu = () => {
@@ -43,10 +47,16 @@ function Navbar() {
             Logout
           </Link>
         )}
-
         <Link to="/cart" className={navStyles.navLink}>
           Cart
         </Link>
+        {isLoggedIn == true && (
+          <Link className={navStyles.navLink}>
+            <div className={navStyles.navProfile}>
+              <CgProfile /> <span>{userName}</span>
+            </div>
+          </Link>
+        )}
         <Link to="/cart" id={navStyles.c} className={navStyles.navLink}>
           <HiShoppingCart /> <span>{data.length}</span>
         </Link>
