722d15570ec58a635a8fab7aeb3e020835156d1d
Janamjay <janamjayk6@gmail.com>
2024-01-07 10:10:44 +0530

    register page created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Nav from "./component/nav/Nav";
 import Footer from "./component/footer/Footer";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
     </>
